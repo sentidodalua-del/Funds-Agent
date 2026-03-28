3db034c021f43802f7e5b13dbf0f20c36051573b
sentidodalua-del <sentidodalua@gmail.com>
2026-03-28 16:26:38 +0000

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Configuração das API keys
-# Substitui os valores abaixo pelas tuas keys reais
-
-ANTHROPIC_API_KEY = "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"
-PERPLEXITY_API_KEY = "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"
